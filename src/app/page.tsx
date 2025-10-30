0f624e4ee7eb439148c0608ac47e2d890d079452
yurism <mascarenhasyuri@hotmail.com>
2025-10-30 09:39:58 -0300

    [Fix] Text in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <section className="text-center space-y-4">
-      <h1 className="text-4xl font-bold">Hi, I’m [Your Name]</h1>
+      <h1 className="text-4xl font-bold">Hi, I’m Yuri M.</h1>
       <p className="text-lg text-gray-600">
-        Cyber Security Governance & Risk Specialist | Secure Software Engineer
+        Cyber Security Governance | Secure Software Engineer
       </p>
       <p>
         I design, build, and maintain <b>secure applications</b> that support{" "}
-        <b>Cyber Security Governance</b> activities across enterprise
-        environments.
+        <b>Cyber Security Governance</b> activities across{" "}
+        <b>enterprise environments</b>.
       </p>
     </section>
   );
